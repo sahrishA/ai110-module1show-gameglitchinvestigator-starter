a3cbad685394ba2c317611db96c20b5fbab9bf9b
sahrishA <sa223@njit.edu>
2026-03-01 21:39:11 -0500

    Update app.py
    Updated the return prompt for too low and too high value selection

--- app.py
+++ app.py
@@ -32,19 +32,21 @@ def parse_guess(raw: str):
 def check_guess(guess, secret):
     if guess == secret:
         return "Win", "🎉 Correct!"
-
+# need to fix the comment from Go HIGHER! to Go LOWER
     try:
         if guess > secret:
-            return "Too High", "📈 Go HIGHER!"
+            return "Too High", "📉 Go LOWER!" 
         else:
-            return "Too Low", "📉 Go LOWER!"
+            return "Too Low", "📈 Go HIGHER!"
     except TypeError:
         g = str(guess)
         if g == secret:
             return "Win", "🎉 Correct!"
+        # need to fix the comment from Go HIGHER! to Go LOWER AND VICE VERSA
         if g > secret:
-            return "Too High", "📈 Go HIGHER!"
-        return "Too Low", "📉 Go LOWER!"
+            return "Too High", "📉 Go LOWER!"
+        else:
+            return "Too Low", "📈Go HIGHER!"
 
 
 def update_score(current_score: int, outcome: str, attempt_number: int):
